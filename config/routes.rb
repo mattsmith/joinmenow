7ec7cfb192e1ed26d20b9fcbb42a21c75b55226a
Matt Smith <mattr.smith@gmail.com>
2012-09-20 16:36:32 -0700

    added interests scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Joinmenow::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   resources :events
+  resources :interests
 
   root :to => 'events#index'
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920225636) do
+ActiveRecord::Schema.define(:version => 20120920233611) do
 
   create_table "events", :force => true do |t|
     t.string   "fb_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "interests", :force => true do |t|
