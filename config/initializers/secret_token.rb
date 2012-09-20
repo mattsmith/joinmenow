fb1a1412203402f25627484354c32e2ab490db6d
Matt Smith <mattr.smith@gmail.com>
2012-09-20 13:15:14 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Joinmenow::Application.config.secret_token = '58b049e69592ae2edc1bb56122fb5db7d120b37530f7982e755a257001c6682f20e8726b52d427eac28930ad49ad403b3e680bb4040359bc30a3fc7ec4d86137'
