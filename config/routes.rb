f49fd9c5fb55f9289c7d1f45bc4204472e3a6094
Matt Smith <mattr.smith@gmail.com>
2012-09-20 14:37:09 -0700

    added events scafffold controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Joinmenow::Application.routes.draw do
   devise_for :users
 
+  resources :events
+
+  root :to => 'events#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
