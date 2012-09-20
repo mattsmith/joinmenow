1aa3e69baa12e91eea5b4cdd3ed874cb71cec572
Matt Smith <mattr.smith@gmail.com>
2012-09-20 15:17:27 -0700

    added name to user and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920215938) do
+ActiveRecord::Schema.define(:version => 20120920221414) do
 
   create_table "events", :force => true do |t|
     t.string   "fb_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20120920215938) do
     t.datetime "updated_at",                             :null => false
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
