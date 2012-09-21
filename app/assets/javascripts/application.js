6f39d8355a6f743ee9f359fedcbfd019273d8006
Matt Smith <mattr.smith@gmail.com>
2012-09-20 18:11:35 -0700

    added sign in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require_tree ./bootstrap/.
 //= require_tree .
---
+++ db/migrate/20120921004745_add_lat_lon_to_event.rb
@@ -0,0 +1,6 @@
+class AddLatLonToEvent < ActiveRecord::Migration
+  def change
+    add_column :events, :lat, :string
+    add_column :events, :lon, :string
+  end
+end
